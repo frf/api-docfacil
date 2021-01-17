bab094652ba42c57fe2209da2eddb7a8a6db154e
Fabio Rocha <fabio@fabiofarias.com.br>
2021-01-16 22:43:46 -0300

    swegger

--- app/Api/Controllers/Controller.php
+++ app/Api/Controllers/Controller.php
@@ -6,7 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+/**
+ * @OA\Info(
+ *    title="Docfacil.me",
+ *    version="1.0.0",
+ *    contact="fabio@fabiofarias.com.br"
+ * )
+ * @OA\Server(
+ *      url=L5_SWAGGER_API_HOST,
+ *      description="Api Develop"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
