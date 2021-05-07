a26912e323443619c93c47daeaddfa870e774678
Fabio Rocha <fabio.farias@tegra.com.br>
2021-05-07 18:07:26 -0300

    supervisor

--- deploy.php
+++ deploy.php
@@ -47,7 +47,7 @@ task('build', function () {
 });
 
 task('supervisor:execute', function () {
-    run('sudo killall supervisord ');
+    run('killall supervisord ');
     run('/usr/bin/supervisord -c /var/www/ajudalocal/shared/supervisor.conf ');
 });
 
