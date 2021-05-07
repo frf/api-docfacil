e724aada75c9de88c6986dc9b391fb3fe5698537
Fabio Rocha <fabio.farias@tegra.com.br>
2021-05-07 18:55:27 -0300

    supervisor

--- deploy.php
+++ deploy.php
@@ -46,15 +46,18 @@ task('build', function () {
     run('cd {{release_path}} && build');
 });
 
-task('supervisor:execute', function () {
-    run('killall -q supervisord ');
+task('supervisor:stop', function () {
+    run('pgrep -x supervisord >/dev/null && killall -q supervisord || echo "OK" ');
+    run('pgrep -x php >/dev/null && killall -q php || echo "OK" ');
+});
+
+task('supervisor:start', function () {
     run('/usr/bin/supervisord -c /var/www/docfacil/shared/supervisor.conf ');
 });
 
 task('supervisor:upload', function() {
     upload('supervisor.conf', '/var/www/docfacil/shared/supervisor.conf');
 });
-
 
 task('deploy', [
     'supervisor:upload',
@@ -80,7 +83,8 @@ after('success', 'slack:notify:success');
 before('deploy', 'slack:notify');
 
 after('deploy', 'success');
-after('success', 'supervisor:execute');
+after('success', 'supervisor:stop');
+after('supervisor:stop', 'supervisor:start');
 
 after('success', 'slack:notify:success');
 after('deploy:failed', 'deploy:unlock');
